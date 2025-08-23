c6d4f46d256fca5d5f41928cc9b2dab734dfe378
Dilisha-KING <technologydilisha@gmail.com>
2025-08-23 19:52:01 +0530

    ★ Ｄ𝙴𝚅𝙴𝙻𝙾𝙿𝙴𝙳 𝙱𝚈 𝙳𝙸𝙻𝙸𝚂𝙷𝙰 𝚃𝙴𝙲𝙷 ㋡

---
+++ plugins/dl-fb.js
@@ -0,0 +1,63 @@
+const axios = require("axios");
+const cheerio = require('cheerio');
+const { cmd, commands } = require('../command')
+const config = require('../config');
+const {fetchJson} = require('../lib/functions');
+
+const api = `https://nethu-api-ashy.vercel.app`;
+
+cmd({
+  pattern: "facebook",
+  react: "🎥",
+  alias: ["fbb", "fbvideo", "fb"],
+  desc: "Download videos from Facebook",
+  category: "download",
+  use: '.facebook <facebook_url>',
+  filename: __filename
+},
+async(conn, mek, m, {
+    from, prefix, q, reply
+}) => {
+  try {
+  if (!q) return reply("🚩 Please give me a facebook url");
+
+  const fb = await fetchJson(`${api}/download/fbdown?url=${encodeURIComponent(q)}`);
+  
+  if (!fb.result || (!fb.result.sd && !fb.result.hd)) {
+    return reply("I couldn't find anything :(");
+  }
+
+  let caption = `*🖥️ 𝐊ꜱᴍ𝐃 𝐅ᴀᴄᴇʙᴏᴏ𝐊 𝐃𝐋*
+
+📝 ＴＩＴＬＥ : 𝙵𝙰𝙲𝙴𝙱𝙾𝙾𝙺 𝚅𝙸𝙳𝙴𝙾
+🔗 ＵＲＬ : ${q}`;
+
+
+  if (fb.result.thumb) {
+    await conn.sendMessage(from, {
+      image: { url: fb.result.thumb },
+      caption : caption,
+      }, mek);
+  }
+
+    if (fb.result.sd) {
+      await conn.sendMessage(from, {
+        video: { url: fb.result.sd },
+        mimetype: "video/mp4",
+        caption: `*✅ 𝙳𝙾𝚆𝙽𝙻𝙾𝙰𝙳𝙴𝙳 𝙰𝚂 𝚂𝙳-𝚀𝚄𝙰𝙻𝙸𝚃𝚈*\n\n📥 𝐊ꜱᴍ𝐃 𝐅𝐁 𝐕ɪᴅᴇ𝐎 𝐃𝐋`
+      }, { quoted: mek });
+    }
+
+if (fb.result.hd) {
+      await conn.sendMessage(from, {
+        video: { url: fb.result.hd },
+        mimetype: "video/mp4",
+        caption: `*✅ 𝙳𝙾𝚆𝙽𝙻𝙾𝙰𝙳𝙴𝙳 𝙰𝚂 𝙷𝙳-𝚀𝚄𝙰𝙻𝙸𝚃𝚈*\n\n📥 𝐊ꜱᴍ𝐃 𝐅𝐁 𝐕ɪᴅᴇ𝐎 𝐃𝐋`
+      }, { quoted: mek });
+    }
+
+} catch (err) {
+  console.error(err);
+  reply("> *ERROR FB CMD IN KSMD BOT*");
+  }
+});
