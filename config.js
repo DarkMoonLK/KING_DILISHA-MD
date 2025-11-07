e58060dac18bd6e9ce25c9f72799d187a73f5cd4
DarkMoonLK <gimshandilisha689@gmail.com>
2025-11-07 11:29:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "KING_DILISHA~NBYCFb6C#eY8oTHwnuwLx0Q2chfWDpxQ_9dLi6VtrZ_hT13mrDH0",
+SESSION_ID: process.env.SESSION_ID || "KING_DILISHA~e50735022d39a15e",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
